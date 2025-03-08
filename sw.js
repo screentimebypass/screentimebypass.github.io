9dcb251f7124eb6aacec8a4e17480bfc32577557
Ben <b3nzo.dev@gmail.com>
2025-03-08 23:18:25 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
 var GHPATH = 'https://screentimebypass.github.io';
-var APP_PREFIX = 'com.molch.screentimebypassyt';
-var VERSION = 'version_001';
+var APP_PREFIX = 'com.molch.screentimebypass';
+var VERSION = 'version_002';
 var URLS = [    
   `${GHPATH}/`,
-  `${GHPATH}/yt.html`,
+  `${GHPATH}/index.html`,
   `${GHPATH}/sw.js`,
   `${GHPATH}/manifest.webmanifest`,
   `${GHPATH}/screentimeicon.png`
