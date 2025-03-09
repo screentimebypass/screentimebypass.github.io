a901d33fa909206ee32870fb5318cf0974ba7af6
Ben <b3nzo.dev@gmail.com>
2025-03-09 15:38:50 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,8 +1,6 @@
 if (navigator.serviceWorker) {
-        navigator.serviceWorker.register (
-          '/sw.js',
-        )
-      }
+    navigator.serviceWorker.register('/sw.js');
+}
 
 let isNavOpen = false;
 
@@ -13,30 +11,26 @@ function toggleNav() {
         openNav();
     }
 }
+
 function openNav() {
-    const sidenav = document.getElementById("mySidenav")
-    const menuButton = document.getElementById("menuButton")
-    const closeButton = document.getElementById("closeButton")
+    const sidenav = document.getElementById("mySidenav");
+    const menuButton = document.getElementById("menuButton");
+    const closeButton = document.getElementById("closeButton");
     isNavOpen = true;
-        sidenav.style.width = "100%";
-        menuButton.src = "https://molchweb.github.io/picture/close.svg";
-        closeButton.style.display = "none";
-    }
+    sidenav.style.width = "100%";
+    menuButton.src = "https://molchweb.github.io/picture/close.svg";
+    closeButton.style.display = "none";
 }
-  function closeNav() {  
-    const sidenav = document.getElementById("mySidenav")
-    const menuButton = document.getElementById("menuButton")
-    const closeButton = document.getElementById("closeButton")
+
+function closeNav() {
+    const sidenav = document.getElementById("mySidenav");
+    const menuButton = document.getElementById("menuButton");
+    const closeButton = document.getElementById("closeButton");
     isNavOpen = false;
-    if (window.innerWidth <= 850) {
-        sidenav.style.width = "0";
-        menuButton.src = "https://molchweb.github.io/picture/menu.svg";
-    } else {
-        sidenav.style.width = "0";
-        setTimeout (() => {
-        }, 300);
-    }
-  }
-  function returnHome() {
-    window.location.href="index.html"
-  }
+    sidenav.style.width = "0";
+    menuButton.src = "https://molchweb.github.io/picture/menu.svg";
+}
+
+function returnHome() {
+    window.location.href = "index.html";
+}
